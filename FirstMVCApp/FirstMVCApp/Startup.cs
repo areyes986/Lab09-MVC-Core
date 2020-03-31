342465a9cd959a0c70d54fb89add058ae211f4e4
Allyson Reyes <areyes986@gmail.com>
2020-03-30 23:45:26 -0700

    set up structure completed

--- FirstMVCApp/FirstMVCApp/Startup.cs
+++ FirstMVCApp/FirstMVCApp/Startup.cs
@@ -16,6 +16,8 @@ namespace FirstMVCApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //adding middleware to used MVC and libraries we are going to use
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +28,22 @@ namespace FirstMVCApp
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+
+                //setting up default routing for MVC app
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
         }
     }
